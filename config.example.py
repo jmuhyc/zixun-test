2b3331a77c4c28563a134b949a14d5b93f2d09fc
洪远琛 <yuanchen_hong@fzzixun.com>
2026-03-24 17:07:27 +0800

    Add config.example.py template

---
+++ config.example.py
@@ -0,0 +1,22 @@
+"""
+API Key 配置文件
+请在此处填写您的各平台 API Key
+"""
+
+# Nano Pro API 配置
+NANO_PRO_API_URL = "http://8.218.228.85:8888/gemini_nano_generate"
+NANO_PRO_API_KEY = ""  # 如果需要 API Key 请在此填写
+
+# 通义万相 (DashScope) API Key
+# 获取地址：https://dashscope.console.aliyun.com/apiKey
+DASHSCOPE_API_KEY = "your_dashscope_key"
+
+# 豆包 Seedream (Volcengine Ark) API Key
+# 获取地址：https://console.volcengine.com/ark/region:ark+cn-beijing/apikey
+ARK_API_KEY = "your_ark_key"
+
+# 腾讯混元 API Key
+# 获取地址：https://console.cloud.tencent.com/cam/capi
+TENCENT_SECRET_ID = "your_secret_id"
+TENCENT_SECRET_KEY = "your_secret_key"
+TENCENT_REGION = "ap-guangzhou"  # 可根据需要修改地域
